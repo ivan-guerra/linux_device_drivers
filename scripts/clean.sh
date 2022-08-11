9f58be8a8d2507b0b00a185667bed96040ca48ac
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2022-08-10 21:55:01 -0700

    Add a project sweeper script.

---
+++ scripts/clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script cleans up the source tree leaving it as if a fresh clone of
+# the repository was made.
+
+# Source the project configuration.
+source config.sh
+
+# Remove the binary directory.
+if [ -d $LINUX_KDEV_BIN_DIR ]
+then
+    echo -e "${LGREEN}Removing '$LINUX_KDEV_BIN_DIR'${NC}"
+    rm -r $LINUX_KDEV_BIN_DIR
+fi
