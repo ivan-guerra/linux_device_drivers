ed97be0c94a41cf829309217c493eeef73b88d32
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2022-08-22 22:49:42 -0700

    Add compile-time enabled debug print macros.

--- modules/scull/scull.h
+++ modules/scull/scull.h
@@ -4,6 +4,23 @@
 #include <linux/cdev.h>
 #include <linux/semaphore.h>
 
+/* Debug macros */
+#undef PDEBUG /* undef it, just in case */
+#ifdef SCULL_DEBUG
+#  ifdef __KERNEL__
+     /* This one if debugging is on, and kernel space */
+#    define PDEBUG(fmt, args...) printk( KERN_DEBUG "scull: " fmt, ## args)
+#  else
+     /* This one for user space */
+#    define PDEBUG(fmt, args...) fprintf(stderr, fmt, ## args)
+#  endif
+#else
+#  define PDEBUG(fmt, args...) /* not debugging: nothing */
+#endif
+
+#undef PDEBUGG
+#define PDEBUGG(fmt, args...) /* nothing: it's a placeholder */
+
 #ifndef SCULL_MAJOR
 #define SCULL_MAJOR 0 /* use dynamic assignment */
 #endif
